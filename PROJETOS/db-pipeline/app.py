81fb50091062dff77ce882c33f0c1fed43d95acb
JeffBeck <jeffmedeiros.jobs@gmail.com>
2024-09-01 17:26:20 -0300

    Trabalho python para engenharia de dados v1.0

--- PROJETOS/db-pipeline/app.py
+++ PROJETOS/db-pipeline/app.py
@@ -10,9 +10,13 @@ load_dotenv()
 
 def data_clean(df, metadados):
     '''
-    Função principal para saneamento dos dados
-    INPUT: Pandas DataFrame, dicionário de metadados
-    OUTPUT: Pandas DataFrame, base tratada
+Função principal para saneamento dos dados
+Args:
+    df (pd.DataFrame): DataFrame do Pandas contendo os dados brutos.
+    metadados (dict): Dicionário de metadados com informações sobre as colunas e tipos de dados.
+
+Returns:
+    pd.DataFrame: DataFrame do Pandas com a base de dados tratada.
     '''
     df["data_voo"] = pd.to_datetime(df[['year', 'month', 'day']]) 
     df = utils.null_exclude(df, metadados["cols_chaves"])
@@ -30,14 +34,41 @@ def data_clean(df, metadados):
     logger.info(f'Saneamento concluído; {datetime.datetime.now()}')
     return df
 
-def feat_eng(df):
-    '''
-    Função ???????????????????????????
-    INPUT: ???????????????????????????
-    OUTPUT: ???????????????????????????
-    '''
+def feat_eng(df, corrige_hr:list, std_str:list, tipos_formatted:dict):
+    """
+Função para realizar a engenharia de features no DataFrame.
+    - Deleta as colunas com sufixo "_formatted" do DataFrame.
+    - Insere os tipos de dados especificados em `tipos_formatted` para as colunas originais do DataFrame.
+
+Args:
+    df (pd.DataFrame): DataFrame do Pandas contendo os dados brutos.
+    corrige_hr (list): Lista das colunas que tiveram correção de horas.
+    std_str (list): Lista das colunas que tiveram padronização de strings.
+    tipos_formatted (dict): Dicionário com os tipos de dados formatados para a tabela final.
+
+Returns:
+    pd.DataFrame: DataFrame do Pandas com as features de engenharia aplicadas.
+    """
+
+    df_work = df.copy()
+
+    # Consolida as listas de canpos
+    formatted_cols_name = std_str + corrige_hr
+    #substitui dados das colunas originais pelas colunas com sufixo _formatted
+    for col in df_work[formatted_cols_name].columns:
+        df_work[col] = df_work[col+'_formatted']
+        # deleta coluna com sufixo "_formatted"
+        df_work.drop(f'{col}_formatted', axis=1, inplace=True)
+    
+    # Formata data type das colunas do df final
+    df_work = utils.convert_data_type(df_work, tipos_formatted)
+    
+
     #colocar log info
-    pass
+    logger.info(
+            f"Finalizado Feature de engenharia Dados ; {datetime.datetime.now()}")        
+    
+    return df_work
 
 def save_data_sqlite(df):
     try:
@@ -50,6 +81,7 @@ def save_data_sqlite(df):
     conn.commit()
     logger.info(f'Dados salvos com sucesso; {datetime.datetime.now()}')
     conn.close()
+
 
 def fetch_sqlite_data(table):
     try:
@@ -70,8 +102,9 @@ if __name__ == "__main__":
     df = pd.read_csv(os.getenv('DATA_PATH'),index_col=0)
     df = data_clean(df, metadados)
     utils.null_check(df, metadados["null_tolerance"])
-    utils.keys_check(df, metadados["cols_chaves"])
-    df = feat_eng(df)
+    utils.keys_check(df, metadados["cols_chaves_renamed"])
+    df = feat_eng(df, metadados['std_str'], metadados['corrige_hr'], metadados['tipos_formatted'])
     #save_data_sqlite(df)
-    fetch_sqlite_data(metadados["tabela"][0])
+    # fetch_sqlite_data(metadados["tabela"][0])
+    df.head(500).to_excel('data/amostragem_saida.xlsx', index=False)
     logger.info(f'Fim da execução ; {datetime.datetime.now()}')
--- PROJETOS/db-pipeline/assets/utils.py
+++ PROJETOS/db-pipeline/assets/utils.py
@@ -8,6 +8,23 @@ logging.basicConfig(filename='data/flights_pipe_log.log', level=logging.INFO)
 logger = logging.getLogger()
 
 def read_metadado(meta_path):
+    '''
+Função para ler metadados de um arquivo Excel e organizar as informações em um dicionário.
+INPUT:\n * <b>meta_path</b> (str): Caminho para o arquivo Excel contendo os metadados.
+OUTPUT:
+    * dict: Dicionário contendo várias informações extraídas dos metadados, incluindo:
+        - tabela (numpy.ndarray): Valores únicos da coluna 'tabela'.
+        - cols_originais (list): Lista das colunas originais.
+        - cols_renamed (list): Lista das colunas renomeadas.
+        - tipos_originais (dict): Dicionário mapeando colunas originais para seus tipos originais.
+        - tipos_formatted (dict): Dicionário mapeando colunas renomeadas para seus tipos formatados.
+        - cols_chaves (list): Lista das colunas chaves originais.
+        - cols_chaves_renamed (list): Lista das colunas chaves renomeadas.
+        - null_tolerance (dict): Dicionário mapeando colunas renomeadas para sua tolerância a nulos.
+        - std_str (list): Lista das colunas renomeadas que precisam de padronização de strings.
+        - corrige_hr (list): Lista das colunas renomeadas que precisam de correção de horário.
+
+    '''
     meta = pd.read_excel(meta_path)
     metadados = {
         "tabela": meta["tabela"].unique(),
@@ -16,6 +33,7 @@ def read_metadado(meta_path):
         "tipos_originais" : dict(zip(list(meta["cols_originais"]),list(meta["tipo_original"]))),
         "tipos_formatted" : dict(zip(list(meta["cols_renamed"]),list(meta["tipo_formatted"]))),
         "cols_chaves" : list(meta.loc[meta["key"] == 1]["cols_originais"]),
+        "cols_chaves_renamed" : list(meta.loc[meta["key"] == 1]["cols_renamed"]),
         "null_tolerance" : dict(zip(list(meta["cols_renamed"]), list(meta["raw_null_tolerance"]))),
         "std_str" : list(meta.loc[meta["std_str"] == 1]["cols_renamed"]),
         "corrige_hr" : list(meta.loc[meta["corrige_hr"] == 1]["cols_renamed"])
@@ -25,11 +43,16 @@ def read_metadado(meta_path):
 # Funções de Saneamento ----------------------------------------------------------------
 
 def null_exclude(df, cols_chaves):
-    '''
-    Função de exclusao das observações nulas
-    INPUT: Pandas DataFrame, lista de colunas que são chaves
-    OUTPUT: Pandas DataFrame com as observações nulas excluidas
-    '''
+    """
+Função de exclusão das observações nulas.
+
+Args:
+    df (pd.DataFrame): DataFrame do Pandas.
+    cols_chaves (list): Lista de colunas que são chaves.
+
+Returns:
+    pd.DataFrame: DataFrame do Pandas com as observações nulas excluídas.
+    """
     tmp = df.copy()
     for col in cols_chaves:
         tmp_df = tmp.loc[~df[col].isna()]
@@ -37,22 +60,33 @@ def null_exclude(df, cols_chaves):
     return tmp_df
 
 def select_rename(df, cols_originais, cols_renamed):
-    '''
-    Função de validação de nulos
-    INPUT: Pandas DataFrame, lista dos nomes das colunas e lista dos novos nomes 
-    OUTPUT: Pandas DataFrame com novos nomes
-    '''
+    """
+Função para renomear colunas.
+
+Args:
+    df (pd.DataFrame): DataFrame do Pandas.
+    cols_originais (list): Lista dos nomes das colunas originais.
+    cols_renamed (list): Lista dos novos nomes das colunas.
+
+Returns:
+    pd.DataFrame: DataFrame do Pandas com os novos nomes das colunas.
+    """
     df_work = df.loc[:, cols_originais].copy()
     columns_map = dict(zip(cols_originais,cols_renamed))
     df_work.rename(columns=columns_map, inplace = True)
     return df_work
 
 def convert_data_type(df, tipos_map):
-    '''
-    Função de validação de nulos
-    INPUT: Pandas DataFrame, dicionário de colunas como chave e seus tipos como valores
-    OUTPUT: Pandas DataFrame com novos nomes
-    '''
+    """
+Função que converte o tipo do dado.
+
+Args:
+    df (pd.DataFrame): DataFrame do Pandas.
+    tipos_map (dict): Dicionário com colunas como chave e seus tipos como valores.
+
+Returns:
+    pd.DataFrame: DataFrame do Pandas com os tipos de dados convertidos.
+    """
     data = df.copy()
     for col in tipos_map.keys():
         tipo = tipos_map[col]
@@ -68,11 +102,18 @@ def convert_data_type(df, tipos_map):
 
 
 def string_std(df, std_str):
-    '''
-    Função de validação de nulos
-    INPUT: Pandas DataFrame, lista das colunas que devem receber a padronização de strings
-    OUTPUT: Pandas DataFrame com as colunas padronizadas
-    '''
+    """
+Função para criar colunas padronizadas.
+
+Adiciona novas colunas com o sufixo _formatted no retorno.
+
+Args:
+    df (pd.DataFrame): DataFrame do Pandas.
+    std_str (list): Lista das colunas que devem receber a padronização de strings.
+
+Returns:
+    pd.DataFrame: DataFrame do Pandas com as colunas padronizadas.
+    """
     df_work = df.copy()
     for col in std_str:
         new_col = f'{col}_formatted'
@@ -81,11 +122,16 @@ def string_std(df, std_str):
 
 # Funções de validação
 def null_check(df, null_tolerance):
-    '''
-    Função de validação de nulos
-    INPUT: Pandas DataFrame, dicionário de colunas como chave e critério de nulo como valores
-    OUTPUT: Pandas DataFrame
-    '''
+    """
+Função de validação de nulos.
+
+Args:
+    df (pd.DataFrame): DataFrame do Pandas.
+    null_tolerance (dict): Dicionário de colunas como chave e critério de nulo como valores.
+
+Returns:
+    pd.DataFrame: DataFrame do Pandas.
+    """
     for col in null_tolerance.keys():
         if  len(df.loc[df[col].isnull()])/len(df)> null_tolerance[col]:
             logger.error(
@@ -93,14 +139,24 @@ def null_check(df, null_tolerance):
         else:
              logger.info(
                 f"{col} possui nulos dentro do esperado; {datetime.datetime.now()}")
-            
-def keys_check(df, cols_chaves):
-    '''
-    Função ???????????????????????????
-    INPUT: ???????????????????????????
-    OUTPUT: ???????????????????????????
-    '''
-    #colocar log info
+
+def keys_check(df, cols_chaves:list):
+    """
+Função para validar se a quantidade de linhas das colunas chaves sem duplicidade é igual ao tamanho do mesmo DataFrame.
+
+Args:
+    df (pd.DataFrame): DataFrame do Pandas.
+    cols_chaves (list): Lista com as colunas chaves.
+
+Returns:
+    None: Mensagem de aviso no log.
+    """
+    if len(df[cols_chaves].drop_duplicates()) == len(df):
+        pass
+    else:
+        #colocar log info
+        logger.info(
+            f"[{len(df.loc[df[cols_chaves].duplicated()])}] Dados duplicados ; {datetime.datetime.now()}")        
     pass
 
 # Funções auxiliares -------------------------------------------
